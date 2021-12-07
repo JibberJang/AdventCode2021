b55955103b9e2407112d9eae2487d09fcdcb2f45
JibberJang <supanova2n@gmail.com>
2021-12-06 21:13:02 -0500

    commit day one

---
+++ AdventCode2021/AdventCode2021/Program.fs
@@ -0,0 +1,37 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.Linq
+open System.IO
+open System.Collections
+
+let tryParseInt s =
+    match Int32.TryParse (s:string) with
+    | true, n -> Some n
+    | false, _ -> None
+
+let readLinesString  = File.ReadLines("../../../Input/dayone.txt")
+let numbers = readLinesString |> Seq.choose tryParseInt |> Seq.sort
+let fileLen = readLinesString.Count()
+
+let findLargerThanPrevious (lines : int seq) : int = 
+    let mutable greaterThanCount = 0
+    let mutable prevLine = 0
+
+    for line in lines do
+        if line > 0 then
+            printf "Is %i greater than %i" line prevLine
+            if line > prevLine
+                then 
+                    greaterThanCount <- greaterThanCount + 1
+                    printfn " yes - the total is %i" greaterThanCount
+            else printfn " no - the total is %i" greaterThanCount
+        prevLine <- line
+
+    greaterThanCount
+    
+
+[<EntryPoint>]
+let main argv =
+    let result = findLargerThanPrevious numbers //Comes up with 1810
+    0 // return an integer exit code
